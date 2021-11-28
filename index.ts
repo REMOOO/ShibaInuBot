f14aa715826ee6ded9155a4c5175066557997450
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-11-28 05:36:54 +0100

    Add slash commands

--- index.ts
+++ index.ts
@@ -11,6 +11,54 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('The bot is ready')
+
+    const commands = client.application?.commands
+
+    commands?.create({
+        name: 'shiba',
+        description: 'Replies with shiba',
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [
+            {
+                name: 'num1',
+                description: 'The first number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+            },
+            {
+                name: 'num2',
+                description: 'The second number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+            },
+        ],
+    })
+})
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
+    }
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'shiba') {
+        interaction.reply({
+            content: 'inu',
+            ephemeral: true,
+        })
+    } else if (commandName === 'add') {
+        const num1 = options.getNumber('num1')!
+        const num2 = options.getNumber('num2')!
+
+        interaction.reply({
+            content: `The sum is ${num1 + num2}`
+        })
+    }
 })
 
 client.on('messageCreate', (message) => {
