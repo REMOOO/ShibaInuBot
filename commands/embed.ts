e08389879ff16920cabc10d6e1a6f1d5c0835292
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-11-28 22:33:27 +0100

    Add embed command

---
+++ commands/embed.ts
@@ -0,0 +1,19 @@
+import { MessageEmbed } from "discord.js";
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Text',
+    description: 'Sends an embed. Only works for administrators.',
+
+    slash: 'both',
+    permissions: ['ADMINISTRATOR'],
+
+    callback: async ({ text }) => {
+        const json = JSON.parse(text)
+
+        const embed = new MessageEmbed(json)
+
+        return embed
+    }
+
+} as ICommand
