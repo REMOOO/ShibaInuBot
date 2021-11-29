9b1ac5a3e517d710345bfc08fc1e6fb549e2c59c
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-11-29 04:49:18 +0100

    Add cat

---
+++ commands/image/cat.ts
@@ -0,0 +1,25 @@
+import { ICommand } from "wokcommands";
+import { MessageEmbed } from "discord.js";
+const getter = require("reddit-api-image-getter")
+
+export default {
+    category: 'Image',
+    description: 'Get an image of a cute kitty',
+
+    slash: 'both',
+
+    callback: async ({}) => {
+        let subreddits = ["catpictures", "catpics"]
+        let subreddit = subreddits[Math.floor(Math.random()*(subreddits.length))]
+
+        let api = new getter()
+
+        const res = await api.getHotImagesOfSubReddit(subreddit)
+        const post = res[Math.floor(Math.random() * res.length)]
+
+        const embed = new MessageEmbed()
+                    .setTitle(post.title)
+                    .setImage(post.url)
+                return embed
+    }
+} as ICommand
