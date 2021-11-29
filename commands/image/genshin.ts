212eee646a2cb37c9adf1816ab94e80694df0503
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-11-29 05:03:09 +0100

    Add genshin

---
+++ commands/image/genshin.ts
@@ -0,0 +1,22 @@
+import { ICommand } from "wokcommands";
+import { MessageEmbed } from "discord.js";
+const getter = require("reddit-api-image-getter")
+
+export default {
+    category: 'Image',
+    description: 'Get a genshin impact related image',
+
+    slash: 'both',
+
+    callback: async ({}) => {
+        let api = new getter()
+
+        const res = await api.getHotImagesOfSubReddit("Genshin_Impact")
+        const post = res[Math.floor(Math.random() * res.length)]
+
+        const embed = new MessageEmbed()
+                    .setTitle(post.title)
+                    .setImage(post.url)
+                return embed
+    }
+} as ICommand
