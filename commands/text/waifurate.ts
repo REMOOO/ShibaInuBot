53757fa932473e4d4d7497d25862dee9ddc74773
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-11-29 04:53:11 +0100

    Add waifurate cmd

---
+++ commands/text/waifurate.ts
@@ -0,0 +1,30 @@
+import { GuildMember, MessageEmbed } from "discord.js";
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Text',
+    description: 'Measure the waifu of a user',
+
+    slash: 'both',
+
+    maxArgs: 1,
+    expectedArgs: '<user>',
+    expectedArgsTypes: ['USER'],
+
+    callback: ({ message, interaction }) => {
+        const target = message ? message.mentions.members?.first() : interaction.options.getMember('user') as GuildMember
+        if (!target) {
+            const embed = new MessageEmbed()
+                .setTitle(`You are ${getWaifuRate()}% waifu 😍`)
+            return embed
+        } else {
+            const embed = new MessageEmbed()
+                .setTitle(`${target.user.username} is ${getWaifuRate()}% waifu 😍`)
+            return embed
+        }
+    }
+} as ICommand
+
+function getWaifuRate() {
+    return Math.floor(Math.random() * 101)
+}
