e756eefb9bf4d9b74ef151098f8b5f9e22e5e9b0
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-11-29 05:14:51 +0100

    Add picture

---
+++ commands/image/picture.ts
@@ -0,0 +1,25 @@
+import { ICommand } from "wokcommands";
+import { MessageEmbed } from "discord.js";
+const getter = require("reddit-api-image-getter")
+
+export default {
+    category: 'Image',
+    description: 'Get a random picture',
+
+    slash: 'both',
+
+    callback: async ({}) => {
+        let subreddits = ["pics", "pic", "images"]
+        let subreddit = subreddits[Math.floor(Math.random()*(subreddits.length))]
+
+        let api = new getter()
+
+        const res = await api.getHotImagesOfSubReddit(subreddit)
+        const post = res[Math.floor(Math.random() * res.length)]
+
+        const embed = new MessageEmbed()
+                    .setTitle(post.title)
+                    .setImage(post.url)
+                return embed
+    }
+} as ICommand
