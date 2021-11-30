2b7b37e5ce241123026dcbcfaeabee09c54cb1b5
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-11-30 13:41:35 +0100

    Remove presence intent

--- index.ts
+++ index.ts
@@ -9,7 +9,6 @@ const client = new DiscordJS.Client({
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_PRESENCES,
     ]
 })
 
