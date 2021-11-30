86764153b69acf7288a80028e68abda6ffb06008
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-11-30 22:49:43 +0100

    Add bunny

---
+++ commands/image/bunny.ts
@@ -0,0 +1,32 @@
+import { ICommand } from "wokcommands";
+import { MessageEmbed } from "discord.js";
+const getter = require("reddit-image-fetcher")
+
+export default {
+    category: 'Image',
+    description: 'Get an image of a cute bunny.',
+
+    slash: 'both',
+
+    callback: async ({}) => {
+        let subreddits = ["rabbits", "bunnies"]
+
+        const res = await getter.fetch({
+            type: 'custom',
+            subreddit: subreddits
+        })
+
+        let title = ""
+
+        if(res[0].title.length > 256) {
+            title = "Cute bunny"
+        } else {
+            title = res[0].title
+        }
+
+        const embed = new MessageEmbed()
+                    .setTitle(title)
+                    .setImage(res[0].image)
+                return embed
+    }
+} as ICommand
