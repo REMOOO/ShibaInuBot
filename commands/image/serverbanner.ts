69904534759c1d2d748889d3dd587adf4d6cc379
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-12-09 02:31:12 +0100

    Add non working server banner

---
+++ commands/image/serverbanner.ts
@@ -0,0 +1,194 @@
+import axios from "axios";
+import { CacheType, CommandInteraction, GuildMember, Message, MessageEmbed, TextChannel } from "discord.js";
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Image',
+    description: 'Get the server banner image of a user.',
+    aliases: ['serverheader'],
+
+    slash: 'both',
+
+    maxArgs: 1,
+    expectedArgs: '<user>',
+    expectedArgsTypes: ['USER'],
+
+    callback: async ({ channel, message, interaction }) => {
+        const target = message ? message.mentions.members?.first() : interaction.options.getMember('user') as GuildMember
+
+        if (!target) {
+            return ownServerBanner(interaction, channel, message)
+        } else {
+            return targetServerBanner(interaction, channel, message, target)
+        }
+    },
+} as ICommand
+
+function ownServerBanner(interaction: CommandInteraction<CacheType>, channel: TextChannel, message: Message<boolean>) {
+    if (!interaction) {
+        return ownMessageServerBanner(channel, message)
+    } else {
+        return ownInteractionServerBanner(channel, interaction)
+    }
+}
+
+async function ownMessageServerBanner(channel: TextChannel, message: Message<boolean>) {
+    if (botHasPermissionsMessage(channel, message)) {
+        const data = await getUserDataMessage(message)
+        console.log(data)
+        return "Will work when Discord fixes server banner data"
+        // if (data.banner) {
+        //     return createOwnMessageEmbedPositive(data, message)
+        // } else {
+        //     if (data.banner_color) {
+        //         return createOwnMessageEmbedNegativeDetail(data)
+        //     } else {
+        //         return createOwnMessageEmbedNegative()
+        //     }
+        // }
+    }
+}
+
+async function getUserDataMessage(message: Message<boolean>) {
+    return await axios.get(`https://discord.com/api/guilds/${message.guildId}/members/${message.author.id}`, {
+        headers: {
+            Authorization: `Bot ${process.env.TOKEN}`
+        }
+    }).then(d => d.data)
+}
+
+function createOwnMessageEmbedPositive(data: any, message: Message<boolean>) {
+    let end = data.banner.startsWith("a_") ? ".gif?size=4096" : ".png?size=4096";
+    const embed = new MessageEmbed()
+        .setTitle('Your banner')
+        .setImage(`https://cdn.discordapp.com/guilds/${message.guildId}/users/${message.author.id}/banners/${data.banner}${end}`)
+    return embed
+}
+
+function createOwnMessageEmbedNegativeDetail(data: any) {
+    const embed = new MessageEmbed()
+        .setTitle("You don't have a server banner 🙄")
+        .setDescription(`Anyway, your banner color is ${data.banner_color}`)
+        .setColor(data.banner_color);
+    return embed;
+}
+
+function createOwnMessageEmbedNegative() {
+    const embed = new MessageEmbed()
+        .setTitle("You don't have a server banner 🙄");
+    return embed;
+}
+
+async function ownInteractionServerBanner(channel: TextChannel, interaction: CommandInteraction<CacheType>) {
+    if (botHasPermissionsInteraction(channel, interaction)) {
+        const data = await getUserDataInteraction(interaction)
+
+        return "Will work when Discord fixes server banner data"
+
+        // if (data.banner) {
+        //     return createOwnInteractionEmbedPositive(data, interaction)
+        // } else {
+        //     if (data.banner_color) {
+        //         return createOwnInteractionEmbedNegativeDetail(data)
+        //     } else {
+        //         return createOwnInteractionEmbedNegative()
+        //     }
+        // }
+    }
+}
+
+async function getUserDataInteraction(interaction: CommandInteraction<CacheType>) {
+    return await axios.get(`https://discord.com/api/guilds/${interaction.guildId}/members/${interaction.user.id}`, {
+        headers: {
+            Authorization: `Bot ${process.env.TOKEN}`
+        }
+    }).then(d => d.data);
+}
+
+function createOwnInteractionEmbedPositive(data: any, interaction: CommandInteraction<CacheType>) {
+    let end = data.banner.startsWith("a_") ? ".gif?size=4096" : ".png?size=4096";
+    const embed = new MessageEmbed()
+        .setTitle('Your banner')
+        .setImage(`https://cdn.discordapp.com/banners/${interaction.user.id}/${data.banner}${end}`);
+    return embed;
+}
+
+function createOwnInteractionEmbedNegative() {
+    const embed = new MessageEmbed()
+        .setTitle("You don't have a server banner 🙄");
+    return embed;
+}
+
+function createOwnInteractionEmbedNegativeDetail(data: any) {
+    const embed = new MessageEmbed()
+        .setTitle("You don't have a server banner 🙄")
+        .setDescription(`Anyway, your banner color is ${data.banner_color}`)
+        .setColor(data.banner_color);
+    return embed;
+}
+
+function targetServerBanner(interaction: CommandInteraction<CacheType>, channel: TextChannel, message: Message<boolean>, target: GuildMember) {
+    if (!interaction) {
+        if (botHasPermissionsMessage(channel, message)) {
+            return "Will work when Discord fixes server banner data"
+            //return createTargetEmbed(target)
+        }
+    } else {
+        if (botHasPermissionsInteraction(channel, interaction)) {
+            return "Will work when Discord fixes server banner data"
+            //return createTargetEmbed(target)
+        }
+    }
+}
+
+async function createTargetEmbed(target: GuildMember) {
+    const data = await getTargetData(target)
+
+            if (data.banner) {
+                return createTargetEmbedPositive(data, target);
+            } else {
+                if (data.banner_color) {
+                    return createTargetEmbedNegativeDetail(target, data);
+                } else {
+                    return createTargetEmbedNegative(target);
+                }
+            }
+}
+
+async function getTargetData(target: GuildMember) {
+    return await axios.get(`https://discord.com/api/guilds/${target.guild.id}/members/${target.user.id}`, {
+        headers: {
+            Authorization: `Bot ${process.env.TOKEN}`
+        }
+    }).then(d => d.data);
+}
+
+function createTargetEmbedNegative(target: GuildMember) {
+    const embed = new MessageEmbed()
+        .setTitle(`${target.user.username} doesn't have a server banner 🙄`);
+    return embed;
+}
+
+function createTargetEmbedNegativeDetail(target: GuildMember, data: any) {
+    const embed = new MessageEmbed()
+        .setTitle(`${target.user.username} doesn't have a server banner 🙄`)
+        .setDescription(`Anyway, the banner color of ${target.user.username} is ${data.banner_color}`)
+        .setColor(data.banner_color);
+    return embed;
+}
+
+function createTargetEmbedPositive(data: any, target: GuildMember) {
+    let end = data.banner.startsWith("a_") ? ".gif?size=4096" : ".png?size=4096";
+    const embed = new MessageEmbed()
+        .setTitle(`Banner of ${target.user.username}`)
+        .setImage(`https://cdn.discordapp.com/banners/${target.user.id}/${data.banner}${end}`);
+    return embed;
+}
+
+function botHasPermissionsInteraction(channel: TextChannel, interaction: CommandInteraction<CacheType>) {
+    return channel.permissionsFor(interaction.guild?.me!).has("SEND_MESSAGES");
+}
+
+function botHasPermissionsMessage(channel: TextChannel, message: Message<boolean>) {
+    return channel.permissionsFor(message.guild?.me!).has("SEND_MESSAGES");
+}
