76e56dc506465d721a88b17acdecf1bd0e300be8
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-12-10 23:47:59 +0100

    Add bitcoin

---
+++ commands/cryptocurrency/bitcoin.ts
@@ -0,0 +1,61 @@
+import { TextChannel, CommandInteraction, CacheType, Message, MessageEmbed } from "discord.js";
+import { ICommand } from "wokcommands";
+const fetch = require('axios')
+
+export default {
+    category: 'Text',
+    description: "Compare Bitcoin to dollars.",
+
+    slash: 'both',
+
+    callback: async ({ interaction, channel, message }) => {
+        if (!interaction) {
+            if (botHasPermissionsMessage(channel, message)) {
+                return bitcoin()
+            }
+        } else {
+            if (botHasPermissionsInteraction(channel, interaction)) {
+                return bitcoin()
+            }
+        }
+    }
+} as ICommand
+
+async function bitcoin() {
+    let res
+    let res2
+
+    try {
+        res = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD`)
+        res2 = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=BTC`)
+    } catch (err) {
+        return "Error"
+    }
+
+    const data = res.data
+    const data2 = res2.data
+
+    const usd = data.USD.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+
+    try {
+        return createEmbed(data2, usd);
+    } catch (err) {
+        return "Bitcoin error. Please report this in the support server if you want this to be fixed :)."
+    }
+}
+
+function createEmbed(data2: any, usd: any) {
+    const embed = new MessageEmbed()
+        .setTitle(`Bitcoin currency`)
+        .setDescription(`1 Bitcoin = ${usd} dollars\n\n1 dollar = ${data2.BTC} Bitcoins`)
+        .setColor("RANDOM");
+    return embed;
+}
+
+function botHasPermissionsInteraction(channel: TextChannel, interaction: CommandInteraction<CacheType>) {
+    return channel.permissionsFor(interaction.guild?.me!).has("SEND_MESSAGES");
+}
+
+function botHasPermissionsMessage(channel: TextChannel, message: Message<boolean>) {
+    return channel.permissionsFor(message.guild?.me!).has("SEND_MESSAGES");
+}
