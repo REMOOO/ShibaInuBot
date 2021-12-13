1e5cd1168b66d66f0f5496d9b42b9397c159d905
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-12-13 23:34:18 +0100

    Fix volume error

--- commands/music/volume.ts
+++ commands/music/volume.ts
@@ -15,6 +15,7 @@ export default {
         const volume  = interaction.options.getInteger("volume")!
 
         if(volume > 100) return interaction.reply({ content: "Can't go higher than 100%. Especially you.", ephemeral: true})
+        if (volume === 0) return interaction.reply({ content: "Shiba never plays music at zero volume", ephemeral: true})
 
         const isConnected = await music.isConnected({
             interaction: interaction
