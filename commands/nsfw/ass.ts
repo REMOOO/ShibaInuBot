a5b4b83ebcd39eebef56709dbf7a7575b3814e3e
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-12-15 02:26:45 +0100

    Add ass

---
+++ commands/nsfw/ass.ts
@@ -0,0 +1,58 @@
+import { ICommand } from "wokcommands";
+import { MessageEmbed } from "discord.js";
+const getter = require("reddit-image-fetcher")
+
+export default {
+    category: 'Image',
+    description: 'See some ass.',
+
+    slash: 'both',
+
+    callback: async ({ channel, guild }) => {
+        console.log(`ass in ${guild?.name}`)
+
+        if (!channel.nsfw) {
+            return "🔞 This command can only be used in NSFW channels."
+        }
+
+        var { title, res } = await getImageFromReddit();
+
+        return ass(title, res)
+    }
+} as ICommand
+
+function ass(title: string, res: any) {
+    return createEmbed(title, res)
+}
+
+function createEmbed(title: string, res: any) {
+    const embed = new MessageEmbed()
+        .setTitle(title)
+        .setImage(res[0].image);
+    return embed;
+}
+
+async function getImageFromReddit() {
+    let subreddits = [
+        "ass",
+        "AssholeBehindThong",
+        "assinthong",
+        "asstastic",
+        "facedownassup",
+        "NSFW_ASS"
+    ];
+
+    const res = await getter.fetch({
+        type: 'custom',
+        subreddit: subreddits
+    });
+
+    let title = "";
+
+    if (res[0].title.length > 256) {
+        title = "ass";
+    } else {
+        title = res[0].title;
+    }
+    return { title, res };
+}
