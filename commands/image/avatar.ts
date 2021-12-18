e2cef941ae046958fe2fa44868f5ed80b6bd263c
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2021-12-18 14:30:15 +0100

    Add avatar

--- commands/image/avatar.ts
+++ commands/image/avatar.ts
@@ -1,64 +1,149 @@
-import { CacheType, CommandInteraction, GuildMember, Message, MessageEmbed, TextChannel, WebhookClient } from "discord.js";
+import { CacheType, CommandInteraction, MessageEmbed, User, WebhookClient } from "discord.js";
 import { ICommand } from "wokcommands";
+import axios from "axios";
 
 export default {
     category: 'Image',
-    description: 'Get the avatar image of a user.',
-    aliases: ['av', 'pfp'],
+    description: 'Get avatar or banner of a user.',
 
-    slash: 'both',
-    
-    maxArgs: 1,
-    expectedArgs: '<user>',
-    expectedArgsTypes: ['USER'],
+    slash: true,
 
-    callback: async ({ message, interaction, guild }) => {
+    options: [
+        {
+            type: 'SUB_COMMAND',
+            name: 'get',
+            description: 'Get the avatar image of a user.',
+            options: [
+                {
+                    name: 'user',
+                    type: 'USER',
+                    description: 'A user in the server.'
+                }
+            ]
+        },
+        {
+            type: 'SUB_COMMAND',
+            name: 'server',
+            description: 'Get the server avatar image of a user.',
+            options: [
+                {
+                    name: 'user',
+                    type: 'USER',
+                    description: 'A user in the server.'
+                }
+            ]
+        },
+        {
+            type: 'SUB_COMMAND',
+            name: 'banner',
+            description: 'Get the banner image of a user.',
+            options: [
+                {
+                    name: 'user',
+                    type: 'USER',
+                    description: 'A user in the server.'
+                }
+            ]
+        },
+        {
+            type: 'SUB_COMMAND',
+            name: 'serverbanner',
+            description: 'Get the server banner image of a user.',
+            options: [
+                {
+                    name: 'user',
+                    type: 'USER',
+                    description: 'A user in the server.'
+                }
+            ]
+        }
+    ],
+
+    callback: async ({ guild, interaction }) => {
+        const subcommand = interaction.options.getSubcommand()
+        let user = interaction.options.getUser('user')
+        if (!user) user = interaction?.user
         const webhook = new WebhookClient({ url: process.env.COMMANDS_URL! })
-        const embed = new MessageEmbed()
-            .setTitle(`avatar in ${guild?.name}`)
-            .setColor('GREEN')
-        await webhook.send({embeds: [embed]})
 
-        const target = message ? message.mentions.members?.first() : interaction.options.getMember('user') as GuildMember
-        if (!target) {
-            return ownAvatar(interaction, message)
-        } else {
-            return targetAvatar(target)
+        const embed = new MessageEmbed()
+            .setTitle(`avatar ${subcommand} ${user.username} in ${guild?.name}`)
+            .setColor('GREEN')
+        await webhook.send({ embeds: [embed] })
+
+        if (subcommand === 'get') {
+            return avatar(user)
+
+        } else if (subcommand === 'server') {
+            return serveravatar(user, interaction)
+
+        } else if (subcommand === 'banner') {
+            return banner(user)
+
+        } else if (subcommand === 'serverbanner') {
+            return serverbanner(user)
+
         }
     }
 } as ICommand
 
-function targetAvatar(target: GuildMember) {
-    return createTargetEmbed(target);
+async function avatar(target: User) {
+    const embed = new MessageEmbed()
+        .setTitle(`Avatar of ${target.username}`)
+        .setImage(`${target.displayAvatarURL({ dynamic: true, size: 4096 })}`);
+    return embed;
 }
 
-function ownAvatar(interaction: CommandInteraction<CacheType>, message: Message<boolean>) {
-    if (!interaction) {
-            return createOwnMessageEmbed(message);
-        
+async function serveravatar(target: User, interaction: CommandInteraction<CacheType>) {
+    const data = await axios.get(`https://discord.com/api/guilds/${interaction.guildId}/members/${target.id}`, {
+        headers: {
+            Authorization: `Bot ${process.env.TOKEN}`
+        }
+    }).then(d => d.data);
+
+    if (data.avatar !== undefined && data.avatar !== null) {
+        let end = data.avatar.startsWith("a_") ? ".gif?size=4096" : ".png?size=4096"
+
+        const embed = new MessageEmbed()
+            .setTitle(`Server avatar of ${target.username}`)
+            .setImage(`https://cdn.discordapp.com/guilds/${interaction.guildId}/users/${target.id}/avatars/${data.avatar}${end}`);
+        return embed;
     } else {
-            return createOwnInteractionEmbed(interaction);
-        
+        const embed = new MessageEmbed()
+            .setTitle(`${target.username} doesn't have a server avatar 🙄`)
+            .setDescription(`Anyway, this is the global avatar of ${target.username}`)
+            .setImage(`${target.displayAvatarURL({ dynamic: true, size: 4096 })}`);
+        return embed;
     }
 }
 
-function createOwnInteractionEmbed(interaction: CommandInteraction<CacheType>) {
-    const embed = new MessageEmbed()
-        .setTitle(`Your avatar`)
-        .setImage(`${interaction.user.displayAvatarURL({ dynamic: true, size: 4096 })}`);
-    return embed;
+async function banner(target: User) {
+    const data = await axios.get(`https://discord.com/api/users/${target.id}`, {
+        headers: {
+            Authorization: `Bot ${process.env.TOKEN}`
+        }
+    }).then(d => d.data);
+
+    if (data.banner) {
+        let url = data.banner.startsWith("a_") ? ".gif?size=4096" : ".png?size=4096";
+        const embed = new MessageEmbed()
+            .setTitle(`Banner of ${target.username}`)
+            .setImage(`https://cdn.discordapp.com/banners/${target.id}/${data.banner}${url}`);
+        return embed;
+    } else {
+        if (data.banner_color) {
+            const embed = new MessageEmbed()
+                .setTitle(`${target.username} doesn't have a banner 🙄`)
+                .setDescription(`Anyway, the banner color of ${target.username} is ${data.banner_color}`)
+                .setColor(data.banner_color);
+            return embed;
+        } else {
+            const embed = new MessageEmbed()
+                .setTitle(`${target.username} doesn't have a banner 🙄`);
+            return embed;
+        }
+    }
 }
 
-function createOwnMessageEmbed(message: Message<boolean>) {
-    const embed = new MessageEmbed()
-        .setTitle(`Your avatar`)
-        .setImage(`${message.author.displayAvatarURL({ dynamic: true, size: 4096 })}`);
-    return embed;
-}
-
-function createTargetEmbed(target: GuildMember) {
-    const embed = new MessageEmbed()
-        .setTitle(`Avatar of ${target.user.username}`)
-        .setImage(`${target.user.displayAvatarURL({ dynamic: true, size: 4096 })}`);
-    return embed;
+async function serverbanner(target: User) {
+    return "Will work when Discord fixes server banner data"
 }
